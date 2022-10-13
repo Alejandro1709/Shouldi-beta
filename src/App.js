30af34965bde06f60feddf6d142ad0058ded48c1
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-13 16:26:16 -0500

    Add: Question Card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import QuestionCard from './components/QuestionCard'
 
 function App() {
   return (
-    <div className='App'>
+    <div className='container mx-auto'>
       <h1>Hello</h1>
+      <div className='flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-4'>
+        <QuestionCard />
+        <QuestionCard />
+        <QuestionCard />
+      </div>
     </div>
   )
 }
