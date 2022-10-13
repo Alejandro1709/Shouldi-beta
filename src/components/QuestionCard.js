f171c6995a9d53cd5310273b407291f47a4286c9
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-13 17:41:29 -0500

    Add: dummy data for now

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,22 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function QuestionCard() {
+function QuestionCard({question}) {
   return (
     <article className="flex select-none flex-col gap-4 md:rounded-md border md:shadow-md p-4">
       <div className="flex items-center gap-2 justify-between">
-        <span className="text-sm text-teal-400">@alejandro.js</span>
-        <p className="font-semibold text-sm md:text-lg text-center">Should i go to the beach tomorrow midnight?</p>
+        <span className="text-sm text-teal-400">@{question.user.username}</span>
+        <p className="font-semibold text-sm md:text-lg text-center">{question.title}</p>
         <span className="text-sm text-gray-400">2 hours ago</span>
       </div>
       <div className="flex items-center justify-center">
-        <p className="text-center text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, beatae magnam incidunt temporibus odio molestias iste culpa libero delectus corporis!</p>
+        <p className="text-center text-gray-500">{question.content}</p>
       </div>
       <div className="flex h-8 flex-row gap-6">
-        <button className="flex-1 rounded bg-teal-400 text-center text-white hover:bg-teal-500">Yes</button>
-        <button className="flex-1 rounded bg-red-400 text-center text-white hover:bg-red-500">No</button>
+        <button className="flex-1 rounded bg-teal-400 text-center text-white hover:bg-teal-500">Yes ({question.upvotes})</button>
+        <button className="flex-1 rounded bg-red-400 text-center text-white hover:bg-red-500">No ({question.downvotes})</button>
       </div>
     </article>
   )
 }
 
 export default QuestionCard
+
+QuestionCard.propTypes = {
+  question: PropTypes.object
+}
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { questions } from '../data'
 import QuestionCard from './QuestionCard'
 
 function QuestionList() {
+  const [allQuestions] = useState(questions)
   return (
     <div className='flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-4 '>
-      <QuestionCard />
-      <QuestionCard />
-      <QuestionCard />
-      <QuestionCard />
+      {allQuestions.map((question) => (
+        <QuestionCard key={question.id} question={question} />
+      ))}
     </div>
   )
 }
