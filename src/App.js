d94557b2f62bdb299626d83b3b5847c09b000cb3
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-13 16:37:04 -0500

    Add: Navbar UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 import QuestionCard from './components/QuestionCard'
 
 function App() {
   return (
     <div className='container mx-auto'>
-      <h1>Hello</h1>
+      <Navbar />
       <div className='flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-4'>
         <QuestionCard />
         <QuestionCard />
