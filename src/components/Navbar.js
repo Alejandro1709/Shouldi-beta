4ead065ea879ef414593c0005abd0dfdb66735a3
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-13 16:58:01 -0500

    Add: State for Open/Close navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'wouter'
 
 function Navbar() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false)
+
   return (
     <>
       <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
@@ -9,11 +11,11 @@ function Navbar() {
           <span className="font-semibold text-xl tracking-tight">Shouldi</span>
         </div>
         <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <button onClick={() => setIsNavbarOpen(!isNavbarOpen)} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
           </button>
         </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        {isNavbarOpen ? <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
             <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
               Feed
@@ -22,7 +24,7 @@ function Navbar() {
               Account
             </Link>
           </div>
-        </div>
+        </div> : null}
       </nav>
     </>
   )
