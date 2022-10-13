be3bf3b487bce8b2f037381e5abc801fc5fb2282
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-13 18:23:33 -0500

    Add: Modal Context

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import FeedPage from './pages/FeedPage'
 import Container from './components/Container'
 import Navbar from './components/Navbar'
 import Modal from './components/Modal'
+import { ModalProvider } from './context/modalContext'
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
     <Container>
-      {isModalOpen ? <Modal onClose={() => setIsModalOpen(false)}>Lok</Modal> : null}
-      <Navbar />
-      <div className='md:mt-4'>
-        <Route path='/' component={FeedPage} />
-        <Route path='/account'>Profile</Route>
-      </div>
+      <ModalProvider value={{ isModalOpen, setIsModalOpen }}>
+        {isModalOpen ? <Modal>Lok</Modal> : null}
+        <Navbar />
+        <div className='md:mt-4'>
+          <Route path='/' component={FeedPage} />
+          <Route path='/account'>Profile</Route>
+        </div>
+      </ModalProvider>
     </Container>
   )
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types'
+import ModalContext from '../context/modalContext'
 
-function Modal({children, onClose}) {
+function Modal({children}) {
+  const {setIsModalOpen} = useContext(ModalContext)
+  
   return (
-    <div className='flex justify-center items-center absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-70' onClick={onClose}>{children}</div>
+    <div className='flex justify-center items-center absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-70' onClick={() => setIsModalOpen(false)}>{children}</div>
   )
 }
 
