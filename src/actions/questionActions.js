d200eee3899bf4507e1e6d42ae7336a3797aaf8c
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-10-15 18:18:04 -0500

    Add: ability to create a question from the modal

--- src/actions/questionActions.js
+++ src/actions/questionActions.js
@@ -1,12 +1,39 @@
+import { FETCH_QUESTIONS, CREATE_QUESTION, FETCH_QUESTIONS_SUCCESS, FETCH_QUESTIONS_ERROR, CREATE_QUESTION_FAIL, CREATE_QUESTION_SUCCESS } from '../constants/questionConstants'
+
 export const handleFetchQuestions = async (dispatch) => {
-  dispatch({ type: 'FETCH_QUESTIONS' })
+  dispatch({ type: FETCH_QUESTIONS })
 
   try {
-    const response = await fetch('https://shouldi-api.onrender.com/api/v1/questions')
+    const response = await fetch('/api/v1/questions')
     const data = await response.json()
 
-    dispatch({ type: 'FETCH_QUESTIONS_SUCCESS', payload: data })
+    dispatch({ type: FETCH_QUESTIONS_SUCCESS, payload: data })
   } catch (err) {
-    dispatch({ type: 'FETCH_QUESTIONS_ERROR', payload: err })
+    dispatch({ type: FETCH_QUESTIONS_ERROR, payload: err })
+  }
+}
+
+export const handleCreateQuestion = async (dataa, dispatch) => {
+  dispatch({ type: CREATE_QUESTION })
+
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(dataa)
+    }
+
+    const response = await fetch('/api/v1/questions', config)
+    const data = await response.json()
+
+    dispatch({ type: CREATE_QUESTION_SUCCESS, payload: [data] })
+
+    return data
+
+  } catch (error) {
+    console.log(error)
+    dispatch({ type: CREATE_QUESTION_FAIL})
   }
 }
--- src/components/CreateQuestionForm.js
+++ src/components/CreateQuestionForm.js
@@ -1,9 +1,33 @@
-import React, { useContext, useRef } from 'react'
+import React, { useState, useContext, useRef } from 'react'
+import { handleCreateQuestion } from '../actions/questionActions'
 import ModalContext from '../context/modalContext'
+import {QuestionContext} from '../context/questionContext'
 
 function CreateQuestionForm() {
-  const {setIsModalOpen} = useContext(ModalContext)
+  const [formData, setFormData] = useState({
+    title: '',
+    content: '',
+    upvotes: 1,
+    downvotes: 1,
+  })
+
+  const [state, setState] = useState({
+    status: 'idle',
+    isSubmitting: false,
+    error: null
+  })
+
+  const { setIsModalOpen } = useContext(ModalContext)
+  const { dispatch } = useContext(QuestionContext)
+
   const buttonRef = useRef()
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
 
   function handleCloseModal(e) {
     if (e.target.dataset.id === buttonRef.current.dataset.id) {
@@ -11,18 +35,41 @@ function CreateQuestionForm() {
     }
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!formData.title || !formData.content) {
+      setState({
+        status: 'error',
+        error: 'Please fill out all fields'
+      })
+      return
+    }
+
+    setState({ ...state, isSubmitting: true })
+
+    handleCreateQuestion(formData, dispatch).then((data) => {
+      console.log(data)
+    }).catch((err) => {
+      console.log(err)
+      setState({ status: 'error', error: err.message })
+    })
+
+  }
+
   return (
     <div className='bg-white md:w-3/12 w-3/4 rounded-lg'>
+      {state.status === 'error' ? <p className='text-red-500'>{state.error}</p> : null}
       <div className='flex flex-col gap-4 p-6 items-start'>
         <button className='p-2 bg-teal-100 hover:bg-teal-200 rounded-lg' data-id='close-btn' onClick={handleCloseModal} ref={buttonRef}>Close</button>
-        <form className='flex flex-col gap-4 w-full'>
+        <form className='flex flex-col gap-4 w-full' onSubmit={handleSubmit}>
           <div className='flex flex-col gap-2'>
             <label htmlFor='title'>Title</label>
-            <input className='p-2 border' type='text' name='title' id='title' placeholder='Should i learn React or Vue?' />
+            <input className='p-2 border' type='text' name='title' id='title' placeholder='Should i learn React or Vue?' value={formData.title} onChange={handleChange} />
           </div>
           <div className='flex flex-col gap-2'>
             <label htmlFor='content'>Content</label>
-            <textarea className='p-2 border' name='content' id='content' placeholder='I am a beginner in web development and i want to learn a framework for building web apps. Which one should i learn first, React or Vue?' rows={4}></textarea>
+            <textarea className='p-2 border' name='content' id='content' placeholder='I am a beginner in web development and i want to learn a framework for building web apps. Which one should i learn first, React or Vue?' rows={4} value={formData.content} onChange={handleChange}></textarea>
           </div>
           <button className='bg-teal-400 p-2 rounded-md text-white hover:bg-teal-500' type='submit'>Submit</button>
         </form>
